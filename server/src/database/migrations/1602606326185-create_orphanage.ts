444f5c21b4ac42ec6b785c625ec67dd893fe61eb
Pedro Moreno <nennogabriel@gmail.com>
2020-10-13 13:26:17 -0300

    give up monorepo

---
+++ server/src/database/migrations/1602606326185-create_orphanage.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class createOrphanage1602606326185 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
