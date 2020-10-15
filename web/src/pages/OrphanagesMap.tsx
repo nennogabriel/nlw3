4271f95dfa75dc61ad63a4903afb478262d9df8e
Pedro Moreno <nennogabriel@gmail.com>
2020-10-14 22:58:46 -0300

    dia 3

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,19 +1,48 @@
-import React from 'react';
-import { FiPlus } from 'react-icons/fi';
+import React, { useEffect, useState } from 'react';
+import { FiArrowRight, FiPlus } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
-import { Map, TileLayer } from 'react-leaflet';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 
-import 'leaflet/dist/leaflet.css';
-import mapMarketingImg from '../images/map-marker.svg';
+import mapMarkerImg from '../images/map-marker.svg';
 
 import '../styles/pages/orphanage-map.css';
+import mapIcon from '../utils/mapIcon';
+import api from '../services/api';
+
+interface Orphanage {
+  id: number;
+  latitude: number;
+  longitude: number;
+  name: string;
+}
 
 function OrphanagesMap() {
+  const [orphanages, setOrphanages] = useState<Orphanage[]>([]);
+
+  useEffect(() => {
+    api.get('orphanages').then((response) => {
+      setOrphanages(response.data);
+    });
+  }, []);
+
+  const [mapInitial, setMapInitial] = useState([-27.2092052, -49.6401092]);
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((navPosition) => {
+        setMapInitial([
+          navPosition.coords.latitude,
+          navPosition.coords.longitude,
+        ]);
+      });
+    }
+  }, []);
+
   return (
     <div id="page-map">
       <aside>
         <header>
-          <img src={mapMarketingImg} alt="Happy" />
+          <img src={mapMarkerImg} alt="Happy" />
 
           <h2>Escolha um orfanato no mapa</h2>
           <p>Muitas crianças estão espereando a sua visita :)</p>
@@ -25,7 +54,7 @@ function OrphanagesMap() {
       </aside>
 
       <Map
-        center={[-20.6646993, -40.5012231]}
+        center={[mapInitial[0], mapInitial[1]]}
         zoom={15}
         style={{ width: '100%', height: '100%' }}
       >
@@ -33,9 +62,29 @@ function OrphanagesMap() {
         <TileLayer
           url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
         />
+
+        {orphanages.map((orphanage) => (
+          <Marker
+            position={[orphanage.latitude, orphanage.longitude]}
+            icon={mapIcon}
+            key={orphanage.id}
+          >
+            <Popup
+              closeButton={false}
+              minWidth={240}
+              maxWidth={240}
+              className="map-popup"
+            >
+              {orphanage.name}
+              <Link to={`/orphanages/${orphanage.id}`}>
+                <FiArrowRight size={20} color="#fff" />
+              </Link>
+            </Popup>
+          </Marker>
+        ))}
       </Map>
 
-      <Link to="#" className="create-orphanage">
+      <Link to="/orphanages/create" className="create-orphanage">
         <FiPlus size={32} color="#fff" />
       </Link>
     </div>
